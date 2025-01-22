b3168eec75f085fa0b1a57eb957c9d9b2054fa63
ysw4697 <ysw4697@naver.com>
2025-01-23 01:18:36 +0900

    플레이어 컨트롤러 생성

---
+++ Assets/1. Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+        }
+    }
+}
