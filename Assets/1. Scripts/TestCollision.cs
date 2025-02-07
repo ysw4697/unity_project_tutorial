a5294a6d734768dfa1e16440150bf2bf5faa297b
ysw4697 <ysw4697@naver.com>
2025-02-07 18:16:59 +0900

    콜리전, 트리거 연습_끝
    레이케스트, 레이어, 태그 등 만져봄

--- Assets/1. Scripts/TestCollision.cs
+++ Assets/1. Scripts/TestCollision.cs
@@ -13,4 +13,23 @@ public class TestCollision : MonoBehaviour
     {
         Debug.Log(other.gameObject.name);
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            // int mask = (1 << 8) | (1 << 9);
+            
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+            }
+        }
+    }
 }
