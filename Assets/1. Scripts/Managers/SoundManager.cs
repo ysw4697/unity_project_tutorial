056a495e0d730634da9b6028ae268fb206ddffcc
ysw4697 <ysw4697@naver.com>
2025-03-11 15:41:07 +0900

    사운드 매니저 구현 중_2

--- Assets/1. Scripts/Managers/SoundManager.cs
+++ Assets/1. Scripts/Managers/SoundManager.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class SoundManager 
 {
-    
+    public void Play(Define.Sound type, string path, float pitch = 1.0f)
+    {
+        if (path.Contains("Sounds/") == false)
+        {
+            path = $"Sounds/{path}";
+        }
+        
+        if (type == Define.Sound.Bgm)
+        {
+            AudioClip audioClip = Managers.Resource.Load<AudioClip>(path);
+            if (audioClip == null)
+            {
+                Debug.LogError($"오디오 클립을 못 찾음: {path}");
+                return;
+            }
+        }
+        else
+        {
+            
+        }
+    }
 }
