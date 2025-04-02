f267b69e9b38616cf3107e35f5def6ad6f7a3f55
ysw4697 <ysw4697@naver.com>
2025-04-02 18:11:16 +0900

    feat: PoolManager 구현 중

--- Assets/1. Scripts/Managers/ResourceManager.cs
+++ Assets/1. Scripts/Managers/ResourceManager.cs
@@ -11,6 +11,7 @@ public class ResourceManager
 
     public GameObject Instantiate(string path, Transform parent = null)
     {
+        // 1. original을 이미 들고 있다면 바로 사용
         GameObject prefab = Load<GameObject>($"Prefabs/{path}");
 
         if (prefab == null)
@@ -19,12 +20,9 @@ public class ResourceManager
             return null;
         }
         
+        // 2. 이미 풀링된 객체가 있는지 확인 후 생성
         GameObject go = Object.Instantiate(prefab, parent);
-        int index = go.name.IndexOf("(Clone)");
-        if (index > 0)
-        {
-            go.name = go.name.Substring(0, index);
-        }
+        go.name = prefab.name;
         
         return go;
     }
@@ -35,6 +33,8 @@ public class ResourceManager
         {
             return;
         }
+        
+        // 만약에 풀링이 필요한 객체라면 풀링 매니저에 위탁
         Object.Destroy(obj);
     }
 }
